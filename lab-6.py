4267aefd6370bfc148bf40f8527b00db0c85b0c9
Venancio Barela <calivee.ca@gmail.com>
2022-05-17 23:37:11 -0700

    finished pseudocode

--- lab-6.py
+++ lab-6.py
@@ -13,7 +13,22 @@ We iterate over using a loop three times and print the results to the user. As a
 """
 
 """
+import random
+num = generate random number
 
+if num is greater than 15,
+    then the result will be "Cherries"
+otherwise if num is > 10,
+    then the result will be "Orange"
+otherwise if num is > 5,
+    then the result will be "Plum"
+otherwise if num is > 2,
+    then the result will be "Melon"
+otherwise if num is > 1,
+    then the result will be "Bell"
+otherwise 
+    the result will be "Bar"
 
-
+loop three times
+    print the output (fruit) to the user
 """
